0c782aa2dada27e7dae2d36a9eb99ba82b740705
rayudusubrahmanyam <43774233+rayudusubrahmanyam@users.noreply.github.com>
2021-08-24 12:07:45 +0530

    Update GlobalVars.groovy

--- src/com/lib_demo_pro/GlobalVars.groovy
+++ src/com/lib_demo_pro/GlobalVars.groovy
@@ -2,7 +2,5 @@
 package com.lib_demo_pro
 
 class GlobalVars {
-   static String registryName = "rayudusubrahmanyam/myrepository";
-   Static String registryCredential = 'DockerHub'
-
+   static String registryName = "rayudusubrahmanyam/myrepository"
 }

757bb918846a9ec64bf0b5c471f102f4d657e46b
Rayudu Subrahmanyam <rayudu21@gmail.com>
2021-08-24 09:52:16 +0530

    added files

---
+++ src/com/lib-demo-pro/GlobalVars.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+package com.lib-demo-pro
+
+class GlobalVars {
+   static String foo = "bar"
+
+   // refer to this in a pipeline using:
+   //
+   // import com.cleverbuilder.GlobalVars
+   // println GlobalVars.foo
+}
